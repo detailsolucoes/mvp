45f83a4c17201c4e9b887f973d1b06f605afd67d
[dyad] <suport.detail@gmail.com>
2026-01-10 17:55:48 -0300

    Reverted all changes back to version d8c7a0a9cb6585ab84411bce8cd88deb34351e6e

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -25,7 +25,7 @@ const SidebarContext = createContext<SidebarContextProps | undefined>(
 export const useSidebar = () => {
   const context = useContext(SidebarContext);
   if (!context) {
-    throw new Error("useSidebar must be useSidebar must be used within a SidebarProvider");
+    throw new Error("useSidebar must be used within a SidebarProvider");
   }
   return context;
 };
